2859b19b8a99d4b2c826ab2b5ee50b0c2519b617
pz-hahne <pz.hahne@gmail.com>
2024-01-21 14:45:58 +0100

    Finalised homework 10

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,40 +1,48 @@
-import tic_tac_toe
+from tic_tac_toe import winning_condition
 
-def winning_condition(board):
-    if "xxx" in board: 
-        return "x"
-    elif "ooo" in board:
-        return "o"
-    elif "-" in board: 
-        return "-"
-    else:  
-        return "!"
-    
 #create at least 5 different tests for the behavior of the function.
 
-def test_winning_condition_x():
-    board = tic_tac_toe.computer_choice('--------------------')
-    assert len(board) == 20
-    assert board.count('x') == 3
-    assert "xxx" in board
+def test_winning_condition():
+    # Test case 1: X wins
+    board = "xxx--ooo"
+    assert winning_condition(board) == "x"
+
+    # Test case 2: O wins
+    board = "xx-oooxx"
+    assert winning_condition(board) == "o"
+
+    # Test case 3: Draw
+    board = "xoxoxoxox"
+    assert winning_condition(board) == "-"
+
+    # Test case 4: No winner yet
+    board = "xx---ooo"
+    assert winning_condition(board) == "!"
+
+    # Test case 5: Empty board
+    board = "---------"
+    assert winning_condition(board) == "!"
 
 #create at least 2 different tests for move function
+from tic_tac_toe import move
 
-def move(board, mark, position):
-  new_board = board[:position] + mark + board[position+1:]
-  return new_board
+def test_move_with_x():
+    # Test case 1: Make a move with "x"
+    initial_board = "---------"
+    new_board = move(initial_board, "x", 2)
+    expected_board = "--x------"
+    assert new_board == expected_board
 
-def test_move_to_empty_space():
-    board = tic_tac_toe.computer_choice('--------------------')
-    assert len(board) == 20
-    assert board.count('x') == 1
-    assert board.count('-') == 19
-    
-
+def test_move_with_o():
+    # Test case 2: Make a move with "o"
+    initial_board = "--x------"
+    new_board = move(initial_board, "o", 5)
+    expected_board = "--xo-----"
+    assert new_board == expected_board
 
 # Answers to questions: 
     # Python module = one file; python package as multiple files/ python packages are made up of modules
     # 
     # Exceptions as a way to program specific kinds of errors into code to anticipate a problem that might come up for the user. 
-    #
-    # Benefits of testing: 
+    # create new exception: create new class that functions as exception, throw: , catch: 
+    # Benefits of testing: ensure smoother running of programmes, realise problems that progammer might not have anticipated, 
